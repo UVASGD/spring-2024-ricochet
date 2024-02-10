7bbb5d842a998381e6428ee2719b02d75b68e3fb
wbbaldwin2023 <BillyBaldwin.BBB2017@gmail.com>
2024-02-10 17:03:52 -0500

    ray gun and movement

--- scripts/CharacterBody2D.gd
+++ scripts/CharacterBody2D.gd
@@ -2,11 +2,12 @@ extends CharacterBody2D
 
 var Speed = 0
 const PlayerFriction = 0.07
-const TopSpeed = 250.0
+const TopSpeed = 200.0
 const SpeedInterval = 15
 const VelocityInterval = 25
 const ShootInterval = 1
 var Shot = false
+
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 #Initializes vectors and values needed for shot calculation
@@ -18,7 +19,7 @@ func _physics_process(delta):
 	var power = 0
 	#If shooting, calculate power based on distance to closest wall/floor, move in direction opposite mouse
 	if Input.is_action_just_pressed("Shoot"):
-		power = calcRayCast(deg)
+		power = coolCalcRayCast(deg)
 		print(power)
 		velocity += -ShootVector*power
 		Shot = true
@@ -32,6 +33,15 @@ func _physics_process(delta):
 	if Shot: #if just fired, ignore x axis interpolation and allow player to fly through the air until landed
 		if is_on_floor():
 			Shot = false
+	elif not is_on_floor():
+		if velocity.x > 0: 
+			if Input.is_action_pressed("walkLeft"):
+				velocity.x = 0
+				velocity.y = 0
+		if velocity.x < 0:
+			if Input.is_action_just_pressed("walkRight"):
+				velocity.x = 0	
+				velocity.y = 0
 	elif direction and is_on_floor():
 		Speed = move_toward(Speed, TopSpeed, SpeedInterval)
 		velocity.x = direction * Speed
@@ -39,6 +49,16 @@ func _physics_process(delta):
 		Speed = move_toward(Speed, 0, SpeedInterval)
 		velocity.x = move_toward(velocity.x, 0, VelocityInterval)
 	move_and_slide()
+	
+func coolCalcRayCast(deg: int) -> int:
+	var power = 0
+	var raycast := $Pivot/Gun2/RayCast2D
+	var origin 	= $Pivot/Gun2/RayCast2D.global_transform.origin
+	if $Pivot/Gun2/RayCast2D.is_colliding():
+		var point = $Pivot/Gun2/RayCast2D.get_collision_point()
+		var distance = origin.distance_to(point)
+		power = (500-distance)
+	return power
 	
 	
 	
@@ -53,41 +73,41 @@ func calcRayCast(deg) -> int:
 		if $RightRayCast.is_colliding():
 			var RightPoint = $RightRayCast.get_collision_point()
 			var RightDistance = Origin.distance_to(RightPoint)
-			RightPower = (500-RightDistance) * (1-(abs(deg-360.0)/360.0))
+			RightPower = (500-RightDistance) #* (1-(abs(deg-360.0)/360.0))
 		if $UpRayCast.is_colliding():
 			var UpPoint = $UpRayCast.get_collision_point()
 			var UpDistance = Origin.distance_to(UpPoint)
-			UpPower = (500-UpDistance) * (1-(abs(deg-270.0)/270.0))
+			UpPower = (500-UpDistance) #* (1-(abs(deg-270.0)/270.0))
 		return RightPower if RightPower > UpPower else UpPower
 	if 180 < deg and deg <= 270:
 		if $LeftRayCast.is_colliding():
 			var LeftPoint = $LeftRayCast.get_collision_point()
 			var LeftDistance = Origin.distance_to(LeftPoint)
-			LeftPower = (500-LeftDistance) * (1-(abs(deg-180.0)/180.0))
+			LeftPower = (500-LeftDistance) #* (1-(abs(deg-180.0)/180.0))
 		if $UpRayCast.is_colliding():
 			var UpPoint = $UpRayCast.get_collision_point()
 			var UpDistance = Origin.distance_to(UpPoint)
-			UpPower = (500-UpDistance) * (1-(abs(deg-270.0)/270.0))
+			UpPower = (500-UpDistance) #* (1-(abs(deg-270.0)/270.0))
 		return LeftPower if LeftPower > UpPower else UpPower
 	if 90 < deg and deg <= 180:
 		if $LeftRayCast.is_colliding():
 			var LeftPoint = $LeftRayCast.get_collision_point()
 			var LeftDistance = Origin.distance_to(LeftPoint)
-			LeftPower = (500-LeftDistance) * (1-(abs(deg-180.0)/180.0))
+			LeftPower = (500-LeftDistance) #* (1-(abs(deg-180.0)/180.0))
 		if $DownRayCast.is_colliding():
 			var DownPoint = $DownRayCast.get_collision_point()
 			var DownDistance = Origin.distance_to(DownPoint)
-			DownPower = (500-DownDistance) * (1-(abs(deg-90.0)/90.0))
+			DownPower = (500-DownDistance) #* (1-(abs(deg-90.0)/90.0))
 		return LeftPower if LeftPower > DownPower else DownPower
 	if 0 <= deg and deg <= 90:
 		if $RightRayCast.is_colliding():
 			var RightPoint = $RightRayCast.get_collision_point()
 			var RightDistance = Origin.distance_to(RightPoint)
-			RightPower = (500-RightDistance) * (abs(deg-90.0)/90.0)
+			RightPower = (500-RightDistance) #* (abs(deg-90.0)/90.0)
 		if $DownRayCast.is_colliding():
 			var DownPoint = $DownRayCast.get_collision_point()
 			var DownDistance = Origin.distance_to(DownPoint)
-			DownPower = (500-DownDistance) * (1-(abs(deg-90.0)/90.0))
+			DownPower = (500-DownDistance) #* (1-(abs(deg-90.0)/90.0))
 		return RightPower if RightPower > DownPower else DownPower
 	return 0
 	
