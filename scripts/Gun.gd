3fbfa6196bf5a8ea390b757f2edf7d026de51add
Myshro <greenfishfungus2@gmail.com>
2024-02-13 17:42:08 -0500

    make gun kill enemy

--- scripts/Gun.gd
+++ scripts/Gun.gd
@@ -1,5 +1,7 @@
 extends Node2D
 
+@onready var killRayCast := $Gun2/KillRayCast2D
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
@@ -8,4 +10,7 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	look_at(get_global_mouse_position())
-
+	var hit_collider : Object = killRayCast.get_collider()
+	if hit_collider and hit_collider.name == "Enemy":
+		if Input.is_action_just_pressed("Shoot"):
+			print("enemy killed")
